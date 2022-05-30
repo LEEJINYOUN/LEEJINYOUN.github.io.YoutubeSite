12778eafce6a33b46e6bb46808b4c8f7e2193f32
LEEJINYOUN <102113808+LEEJINYOUN@users.noreply.github.com>
2022-05-30 16:59:02 +0900

    html, css, js : revise

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
+const moreBtn = document.querySelector('.TitleAndMoreBtn .moreBtn');
+const title = document.querySelector('.TitleAndMoreBtn .title');
 
-
-const ThumbsUp = document.querySelector('.fa-thumbs-up');
-const ThumbsDown = document.querySelector('.fa-thumbs-down');
-const Share = document.querySelector('.fa-share');
-const Plus = document.querySelector('.fa-plus');
-const Flag = document.querySelector('.fa-flag');
-
-let count = 0;
-
-ThumbsUp.addEventListener('click', function(){
-    count++
-    if (count % 2 !== 0){
-        ThumbsUp.style.color = 'blue';
-    }
-    else if (count % 2 === 0){
-        ThumbsUp.style.color = '#606060';
-    }
-})
-
+moreBtn.addEventListener('click', () => {
+    moreBtn.classList.toggle('clicked');
+    title.classList.toggle('clamp');
+});
