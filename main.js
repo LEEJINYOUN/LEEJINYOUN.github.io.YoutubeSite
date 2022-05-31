04bd9da3fbe6efa423754e4f41e0e2f013dadc40
LEEJINYOUN <102113808+LEEJINYOUN@users.noreply.github.com>
2022-05-31 10:30:07 +0900

    html,css,js : classname, details revise

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const moreBtn = document.querySelector('.TitleAndMoreBtn .moreBtn');
 const title = document.querySelector('.TitleAndMoreBtn .title');
 
 moreBtn.addEventListener('click', () => {
-    moreBtn.classList.toggle('clicked');
-    title.classList.toggle('clamp');
+    moreBtn
+        .classList
+        .toggle('clicked');
+    title
+        .classList
+        .toggle('clamp');
 });
